cfe90ad5e361a6c7520e95ffe31241a361cc361e
Hélder Santos <heldersantos1999@gmail.com>
2020-11-24 00:34:18 +0000

    weather v1 pronto + novos componentes

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,7 +1,45 @@
+import { ImagesliderComponent } from './components/views/imageslider/imageslider.component';
+import { ProgressbarComponent } from './components/views/progressbar/progressbar.component';
+import { CalendarComponent } from './components/views/calendar/calendar.component';
+import { WeatherComponent } from './components/views/weather/weather.component';
+import { HomeComponent } from './components/views/home/home.component';
+import { ClockComponent } from './components/views/clock/clock.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CalculatorComponent } from './components/views/calculator/calculator.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'clock',
+    component: ClockComponent
+  },
+  {
+    path: 'weather',
+    component: WeatherComponent
+  },
+  {
+    path: 'calendar',
+    component: CalendarComponent
+  },
+  {
+    path: 'progressbar',
+    component: ProgressbarComponent
+  },
+  {
+    path: 'calculator',
+    component: CalculatorComponent
+  },
+  {
+    path: 'imageslider',
+    component: ImagesliderComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule} from '@angular/material/button';
@@ -13,12 +13,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/template/header/header.component';
 import { NavComponent } from './components/template/nav/nav.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { ClockComponent } from './components/views/clock/clock.component';
+import { HomeComponent } from './components/views/home/home.component';
+import { WeatherComponent } from './components/views/weather/weather.component';
+import { CalendarComponent } from './components/views/calendar/calendar.component';
+import { ProgressbarComponent } from './components/views/progressbar/progressbar.component';
+import { CalculatorComponent } from './components/views/calculator/calculator.component';
+import { ImagesliderComponent } from './components/views/imageslider/imageslider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent
+    NavComponent,
+    FooterComponent,
+    ClockComponent,
+    HomeComponent,
+    WeatherComponent,
+    CalendarComponent,
+    ProgressbarComponent,
+    CalculatorComponent,
+    ImagesliderComponent
   ],
   imports: [
     BrowserModule,
